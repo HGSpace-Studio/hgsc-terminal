9de5e1dd54c825f5d75868f3a87fdafe9a23705b
Leonmmcoset <testmod@qq.com>
2026-05-24 23:25:23 +0800

    SB Android

--- flutter/csac/android/app/build.gradle.kts
+++ flutter/csac/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "ink.jjmm.csacflutter.csac"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13846066"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
